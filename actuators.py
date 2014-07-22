e66afc5f0476cb1d70633ddee3bd993281669631
Gregory Doran <greg.doran@gmail.com>
2014-07-22 22:41:23 +0100

    Added basic script

--- actuators.py
+++ actuators.py
@@ -1,5 +1,12 @@
 __author__ = 'greg'
 
+import RPi.GPIO as GPIO
+
+GPIO.setup(19, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
 
 class Motor(object):
 
@@ -7,16 +14,36 @@ class Motor(object):
     BACKWARDS = 2
     OFF = 3
 
-    def __init__(self):
+    def __init__(self, forwards_pin, backwards_pin):
         self._state = Motor.OFF
         """
         :type int
         """
+        self._forwards_pin = forwards_pin
+        self._backwards_pin = backwards_pin
 
     @property
     def state(self):
         return self._state
 
-    @state.setter(property=state)
-    def set_state(self, value):
+    @state.setter
+    def state(self, value):
+        self.update_state()
         self._state = value
+
+    def update_state(self):
+        GPIO.out(self._forwards_pin, self._state == Motor.FORWARDS)
+        GPIO.out(self._backwards_pin, self._state == Motor.BACKWARDS)
+
+    def forwards(self):
+        self.state = Motor.FORWARDS
+
+    def backwards(self):
+        self.state = Motor.BACKWARDS
+
+    def stop(self):
+        self.state = Motor.OFF
+
+
+left_motor = Motor(19, 21)
+right_motor = Motor(24, 26)
--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 __author__ = 'greg'
 
+import time, RPi.GPIO as GPIO
+import actuators
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+
 print "Running"
+actuators.left_motor.forwards()
+actuators.right_motor.forwards()
+
+time.sleep(1)
+
+actuators.left_motor.stop()
+actuators.right_motor.stop()
+print "Finished"
+
+GPIO.cleanup()
